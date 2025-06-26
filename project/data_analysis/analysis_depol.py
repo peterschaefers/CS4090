0fe18adb6b3a64d6f306ee9b95e8c379290feb02
cs4090 <p.schaefers@student.tudelft.nl>
2025-06-26 11:35:12 +0200

    Added analysis

--- project/data_analysis/analysis_depol.py
+++ project/data_analysis/analysis_depol.py
@@ -2,37 +2,52 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-def open_files():
-    folder_path = "./data"  # Folder path
-    files = [f for f in os.listdir(folder_path) if "depol" in f]
+def open_files(folder_path, noise_type):
+    files = [f for f in os.listdir(folder_path) if noise_type in f]
     full_paths = [os.path.join(folder_path, f) for f in files]
     return full_paths
 
-def analyse(files):
-    data = np.array([np.loadtxt(path) for path in files])
+def analyse():
+    path_321 = '../three_to_one/data'
+    path_dejmps = '../dejmps/data'
+    path_epl = '../epl/data'
 
-    print(data)
+    files_depol_321 = open_files(path_321, 'depol')
+    files_depol_dejmps = open_files(path_dejmps, 'depol')
+    files_depol_epl = open_files(path_epl, 'depol')
 
-    depol_fidelity = data[:,0]
-    results = data[:, 1:-1]
-    num_runs = data[:, -1]
+    files_gate_321 = open_files(path_321, 'gate')
+    files_gate_dejmps = open_files(path_dejmps, 'gate')
+    files_gate_epl = open_files(path_epl, 'gate')
 
-    arg_sort = np.argsort(depol_fidelity)
-    depol_fidelity = depol_fidelity[arg_sort]
-    results = results[arg_sort]
-    num_runs = num_runs[arg_sort]
+    data_depol_321 = np.array([np.loadtxt(path) for path in files_depol_321])
+    data_depol_dejmps = np.array([np.loadtxt(path) for path in files_depol_dejmps])
+    data_depol_epl = np.array([np.loadtxt(path) for path in files_depol_epl])
 
-    mean_meas_fids = np.mean(results,axis=1)
+    data_gate_321 = np.array([np.loadtxt(path) for path in files_gate_321])
+    data_gate_dejmps = np.array([np.loadtxt(path) for path in files_gate_dejmps])
+    data_gate_epl = np.array([np.loadtxt(path) for path in files_gate_epl])
+
+    fidelity_depol_321 = data_depol_321[:,0]
+    res_fidelity_depol_321 = data_depol_321[:,1:-1]
+    num_runs_depol_321 = data_depol_321[:,-1]
+
+    arg_sort = np.argsort(fidelity_depol_321)
+
+    fidelity_depol_321 = fidelity_depol_321[arg_sort]
+    res_fidelity_depol_321 = res_fidelity_depol_321[arg_sort]
+    num_runs_depol_321 = num_runs_depol_321[arg_sort]
+
+    av_res_fidelity_depol_321 = np.mean(res_fidelity_depol_321,axis=1)
 
 
-    plt.plot(depol_fidelity, mean_meas_fids,".")
+    plt.plot(fidelity_depol_321, av_res_fidelity_depol_321,c='blue')
     plt.grid()
     plt.xlabel(r'F$_\mathrm{depol}$')
     plt.ylabel(r"$\bar{\mathrm{F}}_\mathrm{dist}$")
-    plt.savefig("figures/avg_fidelity_depol_sweep_50_successes.png", dpi=500)
+    plt.savefig("figures/test.png")
 
 
 if __name__ == "__main__" :
-    files = open_files()
-    analyse(files)
+    analyse()
 
